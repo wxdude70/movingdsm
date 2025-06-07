5028a36208dcfa63e093e5aa0a1d8168bd5d7194
wxdude70 <wxdude@gmail.com>
2025-06-07 18:43:26 -0500

    Update LandingPage.jsx

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,1 +1,38 @@
-<full code already provided in the canvas>
+export default function LandingPage() {
+  return (
+    <>
+      <head>
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"></script>
+        <script dangerouslySetInnerHTML={{ __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-XXXXXXXXXX');
+        `}} />
+
+        {/* Meta Pixel Code */}
+        <script dangerouslySetInnerHTML={{ __html: `
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', 'XXXXXXXXXXXXXXXX');
+          fbq('track', 'PageView');
+        `}} />
+        <noscript>
+          <img height="1" width="1" style={{display: 'none'}} src="https://www.facebook.com/tr?id=XXXXXXXXXXXXXXXX&ev=PageView&noscript=1" />
+        </noscript>
+      </head>
+
+      <div className="min-h-screen bg-white text-gray-800 flex flex-col">
+        <header className="bg-blue-600 text-white p-6 shadow-lg">
+          <h1 className="text-3xl font-bold">Get Free Moving Quotes in Des Moines</h1>
+          <p className="text-lg">Compare rates from trusted local movers</p>
+        </header>
+
+        <main className="flex flex-col md:flex-row items
