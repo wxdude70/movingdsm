4a73ee68a23605a8bddb36349b1b852ef0044738
wxdude70 <wxdude@gmail.com>
2025-06-07 20:31:28 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import LandingPage from './LandingPage';
+import './index.css'; // or wherever your Tailwind/global styles are
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
